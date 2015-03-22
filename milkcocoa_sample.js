5fad9968a87f6ab22e35c266ab26813b86c5ef6f
Shinichiro Suzuki <shinichiro.su@gmail.com>
2015-03-22 18:37:10 +0900

    パラメタでセッション名指定できるようにした

--- milkcocoa_sample.js
+++ milkcocoa_sample.js
@@ -1,19 +1,12 @@
 var milkcocoa = new MilkCocoa("https://io-bi74939df.mlkcca.com");
 var ds = milkcocoa.dataStore("doubutsuShogi");
-// var textArea, board;
-// window.onload = function(){
-//   textArea = document.getElementById("msg");
-//   board = document.getElementById("board");
-// }
 
 function clickEvent(){
-//  var text = textArea.value;
   sendText(text);
 }
 
 function sendText(text){
   ds.push({message : text},function(data){
     console.log("送信完了!");
-//    textArea.value = "";
   });
 }
